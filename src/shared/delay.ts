774726dc82bd42957e622ac876b6a01914a90729
kyusikko <kokyusik91@naver.com>
2024-04-01 18:23:11 +0900

    [#1] feat: 공통 유틸함수 디렉토리 생성

---
+++ src/shared/delay.ts
@@ -0,0 +1,6 @@
+/** @tossdocs-ignore */
+export function delay(milliseconds: number) {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
---
+++ src/shared/shuffle.ts
@@ -0,0 +1,18 @@
+/** @tossdocs-ignore */
+export function shuffle<T>(array: T[]): T[] {
+  let currentIndex = array.length;
+  let randomIndex;
+  const newArray = Array.from(array);
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [newArray[currentIndex], newArray[randomIndex]] = [
+      newArray[randomIndex]!,
+      newArray[currentIndex]!,
+    ];
+  }
+
+  return newArray;
+}
